cecbc053d6edbfd73845fa0352aa26ffcab3c6dc
erikaksland11 <erikaksland11@augustana.edu>
2015-04-08 16:32:51 -0500

    Added new activities and linked them

--- app/src/main/java/edu/augustana/csc490/colormatch/MainActivity.java
+++ app/src/main/java/edu/augustana/csc490/colormatch/MainActivity.java
@@ -8,10 +8,10 @@ package edu.augustana.csc490.colormatch;
 //  timed!
 //  Play any of four difficulty levels: Easy, Medium, Hard, Expert
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -22,26 +22,16 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    // Called when the user clicks the playGameButton button
+    public void showSelectDifficultyMenu(View view) {
+        Intent intent = new Intent(this, SelectDifficultyMenu.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    // Called when the user clicks the highScoresButton button
+    public void showHighScoreMenu(View view) {
+        Intent intent = new Intent(this, HighScoreMenu.class);
+        startActivity(intent);
     }
+
 }
