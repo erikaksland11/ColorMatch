41836a368d3cfa0e2b13a27c9d532fe2ed839de0
erikaksland11 <erikaksland11@augustana.edu>
2015-04-08 21:41:07 -0500

    made radio buttons works, added more connections

--- app/src/main/java/edu/augustana/csc490/colormatch/SelectDifficultyMenu.java
+++ app/src/main/java/edu/augustana/csc490/colormatch/SelectDifficultyMenu.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 
 public class SelectDifficultyMenu extends ActionBarActivity {
@@ -17,6 +18,37 @@ public class SelectDifficultyMenu extends ActionBarActivity {
     // Called when the user clicks the homeButton
     public void showMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // To check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.easyDifficulty:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.mediumDifficulty:
+                if (checked)
+                    // Ninjas rule
+                    break;
+            case R.id.hardDifficulty:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.expertDifficulty:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
+
+
+    // Called when the user clicks the startGameButton
+    public void showGameLayout(View view) {
+        Intent intent = new Intent(this, ThreeButtonGameLayout.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/augustana/csc490/colormatch/ThreeButtonGameLayout.java
@@ -0,0 +1,16 @@
+package edu.augustana.csc490.colormatch;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class ThreeButtonGameLayout extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_button_game_layout);
+    }
+
+
+}
